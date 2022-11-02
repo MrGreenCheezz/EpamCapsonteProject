a15f46514d28b42dfc062be42d27516bb6819ed3
MrGreenCheezz <anonsandro@gmail.com>
2022-11-03 05:04:14 +0600

    added react with bootstrap

---
+++ capstone-project-front/src/Components/TodoItemCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class TodoItemCard extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='card'>
+            <div className='card-body'>
+                <h1 className='card-title'>TestCard</h1>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
